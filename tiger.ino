0e0dc75f20dd935e6a8e249e013fdb4ed3abc3b4
spuzzelsnest <j.mpdesmet@gmail.com>
2019-03-03 15:02:40 +0100

    read sensor

--- tiger.ino
+++ tiger.ino
@@ -1,19 +1,13 @@
 #define LFSensor_1 A0 //line follow sensor1
 #define LFSensor_2 A1 //line follow sensor2
 
-void read_sensor_values() {
-
-  sensor[0]=digitalRead(LFSensor_1);
-  sensor[1]=digitalRead(LFSensor_2);
-}
-
 void setup() {
   /*line follow sensors */
-  pinMode(LFSensor_1,INPUT);
-  pinMode(LFSensor_2,INPUT); 
-  Serial.begin(9600);
+    sensor[0]=digitalRead(LFSensor_1);
+    sensor[1]=digitalRead(LFSensor_2);
+    Serial.begin(9600);
 }
 
 void loop() {
-    Serial.print(sensor[0]);
+    Serial.println("");
 }
